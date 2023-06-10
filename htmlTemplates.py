220d7fc0b1b61504e3a07232d848e626ff6eff2f
elnegmelnegm <48047676+elnegmelnegm@users.noreply.github.com>
2023-06-10 10:32:01 +0300

    htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
+        <img src="https://drive.google.com/drive/folders/1ZLuXO11nK0dZoi0uk83H_qn77IfEOfo2">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
