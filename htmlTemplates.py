fa732f0fae3a81dd6de9c5a030df74be41dfe017
elnegmelnegm <48047676+elnegmelnegm@users.noreply.github.com>
2023-07-07 17:18:29 +0300

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -28,7 +28,7 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="https://www.edaegypt.gov.eg/media/wc3lsydo/group-287.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
